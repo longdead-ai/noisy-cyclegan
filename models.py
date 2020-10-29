40d100a2f448829f76e999f4a4fc2478e9351fa2
Dark Matter <56235355+darkMatter00@users.noreply.github.com>
2020-10-29 20:22:54 +0530

    Added trans resblock

--- models.py
+++ models.py
@@ -7,9 +7,20 @@ from tensorflow.keras import layers, Model, Sequential
 class ResBlock(layers.Layer):
     
     def __init__(self, filters):
-        self.conv1 = layers.Conv2D(filters,kernel_size=4, strides=2,padding='same', use_bias=False)
+        self.conv = layers.Conv2D(filters,kernel_size=4, strides=2,padding='same', use_bias=False)
         self.batchnorm = layers.BatchNormalisation()
         self.relu = layers.ReLU()
         
     def call(self, x):
-        return x + self.relu(self.batchnorm(self.conv1(x)))
+        return self.relu(self.batchnorm(self.conv1(x)))
+
+
+class TransResBlock(layers.Layer):
+    
+    def __init__(self, filters):
+        self.convtrans = layers.Conv2DTranspose(filters, kernel_size=4, strides=2, padding='same', use_bias=False)
+        self.batchnorm = layers.BatchNormalisation()
+        self.relu = layers.ReLU()
+        
+    def call(self, x):
+        return self.relu(self.batchnorm(self.conv(x)))
